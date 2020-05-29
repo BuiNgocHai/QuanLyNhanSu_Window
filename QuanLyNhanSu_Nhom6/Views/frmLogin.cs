646fd3f71bad975e4af108c7c69b13ab415b22e3
TenGiChoHay <tranquangtruong1503@gmail.com>
2020-05-30 01:02:28 +0700

    create frmLogin

--- QuanLyNhanSu_Nhom6/Views/frmLogin.cs
+++ QuanLyNhanSu_Nhom6/Views/frmLogin.cs
@@ -16,5 +16,15 @@ namespace QuanLyNhanSu_Nhom6
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
     }
 }
