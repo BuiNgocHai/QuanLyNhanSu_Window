c7275be2a952dc582b1344644528f1b241f95b77
haibuai3@gmail.com <haibuai3@gmail.com>
2020-06-13 02:46:36 +0700

    add form

---
+++ QuanLyNhanSu_Nhom6/Hai/frmInThongKeTrinhDoNgoaiNgu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.Hai
+{
+    public partial class frmInThongKeTrinhDoNgoaiNgu : Form
+    {
+        public frmInThongKeTrinhDoNgoaiNgu()
+        {
+            InitializeComponent();
+        }
+    }
+}
