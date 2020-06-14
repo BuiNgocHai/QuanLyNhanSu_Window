ae0462d449790bb073533f1c60e058a4afe93ea3
PhanThanhTrung <trung99nb@gmail.com>
2020-06-14 17:44:08 +0700

    add PhongBan_DTO class

---
+++ DTO/PhongBan_DTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class PhongBan_DTO
+    {
+        private string maPhongBan { get; set; }
+        private string tenPhongBan { get; set; }
+        private DateTime ngayThanhLap { get; set; }
+
+        public PhongBan_DTO()
+        {
+
+        }
+        public PhongBan_DTO(string mapb,string tenpb, DateTime ntl)
+        {
+            this.maPhongBan = mapb;
+            this.tenPhongBan = tenpb;
+            this.ngayThanhLap = ntl;
+        }
+    }
+}
